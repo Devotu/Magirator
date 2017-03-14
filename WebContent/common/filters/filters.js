bf8c6eb1a606a2a5bb191fb0e60508867f76dff3
DevotuOne <devotu.developer@gmail.com>
2017-03-14 23:42:49 +0100

    Tags VII
    Moved tags filter up to repeat
    Array of Participants at Commit seems to be an object and thus does not work with this filter.

--- WebContent/common/filters/filters.js
+++ WebContent/common/filters/filters.js
@@ -1,15 +1,25 @@
 ratorApp.filter('postiveTags', function(){
-	return function(tag){
-		if (tag.polarity == 1){
-			return tag.tag;
+	return function(tags){
+		var positive = [];
+		for (var i = 0; i < tags.length; i++) {
+		      var tag = tags[i];
+		      if (tag.polarity == 1) {
+		    	  positive.push(tag.tag);
+		      }
 		}
+		return positive;
 	};
 });
 
 ratorApp.filter('negativeTags', function(){
-	return function(tag){
-		if (tag.polarity == -1){
-			return tag.tag;
+	return function(tags){
+		var negative = [];
+		for (var i = 0; i < tags.length; i++) {
+		      var tag = tags[i];
+		      if (tag.polarity == -1) {
+		    	  negative.push(tag.tag);
+		      }
 		}
+		return negative;
 	};
 });
