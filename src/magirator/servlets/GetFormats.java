5eaa49decbfe759f21fa2305fec8063269f76af9
DevotuOne <devotu.developer@gmail.com>
2017-05-07 21:09:56 +0200

    #49
    All standard formats and the OT special

--- src/magirator/servlets/GetFormats.java
+++ src/magirator/servlets/GetFormats.java
@@ -25,11 +25,40 @@ public class GetFormats extends HttpServlet {
 		getServletContext().log("-- GetFormats --");
 		
         JsonArray returnJson = new JsonArray();
+        //Most popular
         returnJson.add(new JsonPrimitive("Standard"));
+        returnJson.add(new JsonPrimitive("Booster draft"));
+        returnJson.add(new JsonPrimitive("Sealed deck"));
         returnJson.add(new JsonPrimitive("Block"));
-        returnJson.add(new JsonPrimitive("Limited"));
+        
+        //Constructed
+        //returnJson.add(new JsonPrimitive("Standard"));
         returnJson.add(new JsonPrimitive("Modern"));
+        returnJson.add(new JsonPrimitive("Commander"));
+        returnJson.add(new JsonPrimitive("Legacy"));
         returnJson.add(new JsonPrimitive("Vintage"));
+        returnJson.add(new JsonPrimitive("Team unified constructed"));
+        //returnJson.add(new JsonPrimitive("Block"));
+        
+        
+        //Limited
+        //returnJson.add(new JsonPrimitive("Booster draft"));
+        //returnJson.add(new JsonPrimitive("Sealed deck"));
+        returnJson.add(new JsonPrimitive("Conspiracy"));
+        returnJson.add(new JsonPrimitive("Team Booster draft"));
+        returnJson.add(new JsonPrimitive("Team Sealed deck"));
+        
+        //Online
+        returnJson.add(new JsonPrimitive("Freeform"));
+        returnJson.add(new JsonPrimitive("Momir"));
+        returnJson.add(new JsonPrimitive("Pauper"));
+        returnJson.add(new JsonPrimitive("Planeswalker"));
+        returnJson.add(new JsonPrimitive("Duels"));
+        
+        //Custom
+        returnJson.add(new JsonPrimitive("Custom"));
+        returnJson.add(new JsonPrimitive("Booster handover"));
+        
         res.setContentType("application/json");
         res.getWriter().write(returnJson.toString());
 
